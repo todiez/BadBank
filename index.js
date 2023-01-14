ab5a1450d0f4f1b22e58c6f898ea131d890e6a0a
tybo-dev <tobias@diez.guru>
2023-01-14 14:19:20 +0100

    navbar added

--- index.js
+++ index.js
@@ -1,23 +1,14 @@
 function Spa() {
   return (
-    <HashRouter>
-      <NavBar/>
-      <UserContext.Provider value={{users:[{name:'abel',email:'abel@mit.edu',password:'secret',balance:100}]}}>
-        <div className="container" style={{padding: "20px"}}>
-          <Route path="/" exact component={Home} />
-          <Route path="/CreateAccount/" component={CreateAccount} />
-          <Route path="/login/" component={Login} />
-          <Route path="/deposit/" component={Deposit} />
-          <Route path="/withdraw/" component={Withdraw} />
-          <Route path="/balance/" component={Balance} />
-          <Route path="/alldata/" component={AllData} />
-        </div>
-      </UserContext.Provider>      
-    </HashRouter>
+    <>
+    <h1>Welcome to Bad Bank</h1>
+    <NavBar/>
+    </>
   );
 }
 
 ReactDOM.render(
-  <Spa/>,
-  document.getElementById('root')
-);
+  <Spa />,
+  document.getElementById("root")
+
+)
