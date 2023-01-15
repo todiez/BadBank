912a2b584fd9fed62953c048f4ec1d0c1e9cfe13
tybo-dev <tobias@diez.guru>
2023-01-15 16:24:45 +0100

    login added

--- login.js
+++ login.js
@@ -1,13 +1,71 @@
 function Login() {
-      //access shared context with help of react native function useContext and reference
-  //to the created context with UserContext
+  const [show, setShow] = React.useState(true);
+  const [status, setStatus] = React.useState("");
+  const [name, setName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [login, setLogin] = React.useState(false);
+
   const ctx = React.useContext(UserContext);
+  console.log(ctx);
+  // console.log(ctx.users[0].name);
+
+  function handleLogin() {
+    console.log(email);
+    for (let i = 0; i < ctx.users.lenght; i++) {
+      if (ctx.users[i].email === email) {
+        setLogin(true);
+        return;
+      } else {
+        console.log("user does not exist");
+      }
+    }
+  }
 
   return (
-    <h1>
-      Login
-      <br />
-      {JSON.stringify(ctx)}
-    </h1>
+    <Card
+      txtcolor="black"
+      header="Login Page"
+      status={status}
+      body={
+        !login ? (
+          <>
+            E-Mail Address
+            <br />
+            <input
+              type="input"
+              className="form-control"
+              id="email"
+              placeholder="Enter email"
+              value={email}
+              onChange={(e) => setEmail(e.currentTarget.value)}
+            />
+            <br />
+            Password
+            <br />
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              placeholder="Enter password"
+              value={password}
+              onChange={(e) => setPassword(e.currentTarget.value)}
+            />
+            <br />
+            <button
+              type="submit"
+              className="btn btn-light"
+              onClick={handleLogin}
+            >
+              Login
+            </button>
+          </>
+        ) : (
+          <>
+            <h5>Login Success</h5>
+          </>
+        )
+      }
+    />
   );
 }
