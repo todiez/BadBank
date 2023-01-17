f21c4cce4b2e42eb4ea36a6112ad0a2e6e0d700e
tybo-dev <tobias@diez.guru>
2023-01-17 16:29:32 +0100

    deposit function done

--- deposit.js
+++ deposit.js
@@ -1,56 +1,56 @@
 function Deposit() {
-    //access shared context with help of react native function useContext and reference
+  //access shared context with help of react native function useContext and reference
   //to the created context with UserContext
   const ctx = React.useContext(UserContext);
 
   const [deposit, setDeposit] = React.useState(0);
+  const [balance, setBalance] = React.useState(ctx.users[0].balance)
 
-
-  function handleDeposit(deposit) {
+  function handleDeposit(e) {
     console.log("inside handleDeposit: " + deposit);
-    
-    
-      ctx.users[0].balance += deposit;
-      console.log(ctx.users[0].balance);
-    
+    console.log(typeof(deposit))
+
+    if (typeof(deposit) === 'string') {
+      console.log("deposit is a string")
+      let depositInt = parseInt(deposit);
+      setBalance(balance + depositInt);
+    } else {
+      setBalance(balance + deposit);
+    }
   }
 
   return (
     <Card
       txtcolor="black"
       header="Deposit"
-      title={
-        ctx.users[0].login ? (
-          <>            
-            {ctx.users[0].name}
-          </>
-        ) : (
-          <>Please Log in</>
-        )
-      }
+      title={ctx.users[0].login ? <>{ctx.users[0].name}</> : <>Please Log in</>}
       body={
-        ctx.users[0].login ? (
-          <> 
-          Balance: {ctx.users[0].balance} USD
-          <br />
-            Deposit Amount
-            <hr />
-            <input
-              type="number"
-              className="form-control"
-              id="deposit"
-              placeholder="Deposit Amount"
-              value={deposit} 
-              onChange={(e) => setDeposit(e.currentTarget.value)}
-              
-            />
+        !ctx.users[0].login ? (
+          <>
+            Balance: {balance} USD
             <br />
-            <button
-              type="submit"
-              className="btn btn-light"
-              onClick={handleDeposit(deposit)}            >
-              Deposit
-            </button>
+            <hr />
+            <form>
+              <div className="form-group">
+                <label htmlFor="deposit">Deposit Amount:</label>
+                <input
+                  type="number"
+                  className="form-control"
+                  id="deposit"
+                  min="0"
+                  max="99999999"
+                  value={deposit}
+                  onChange={(e) => setDeposit(e.currentTarget.value)}                 
+                />
+              </div>
+              <button
+                type="submit"
+                className="btn btn-light"
+                onClick={handleDeposit}
+              >
+                Submit
+              </button>
+            </form>
           </>
         ) : (
           <></>
