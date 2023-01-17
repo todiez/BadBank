95319f08077f96d5e4ef080a59eab165b78218d9
tybo-dev <tobias@diez.guru>
2023-01-17 14:28:25 +0100

    deoposit updates

--- deposit.js
+++ deposit.js
@@ -3,11 +3,59 @@ function Deposit() {
   //to the created context with UserContext
   const ctx = React.useContext(UserContext);
 
+  const [deposit, setDeposit] = React.useState(0);
+
+
+  function handleDeposit(deposit) {
+    console.log("inside handleDeposit: " + deposit);
+    
+    
+      ctx.users[0].balance += deposit;
+      console.log(ctx.users[0].balance);
+    
+  }
+
   return (
-    <h1>
-      Deposit
-      <br />
-      {JSON.stringify(ctx)}
-    </h1>
+    <Card
+      txtcolor="black"
+      header="Deposit"
+      title={
+        ctx.users[0].login ? (
+          <>            
+            {ctx.users[0].name}
+          </>
+        ) : (
+          <>Please Log in</>
+        )
+      }
+      body={
+        ctx.users[0].login ? (
+          <> 
+          Balance: {ctx.users[0].balance} USD
+          <br />
+            Deposit Amount
+            <hr />
+            <input
+              type="number"
+              className="form-control"
+              id="deposit"
+              placeholder="Deposit Amount"
+              value={deposit} 
+              onChange={(e) => setDeposit(e.currentTarget.value)}
+              
+            />
+            <br />
+            <button
+              type="submit"
+              className="btn btn-light"
+              onClick={handleDeposit(deposit)}            >
+              Deposit
+            </button>
+          </>
+        ) : (
+          <></>
+        )
+      }
+    />
   );
 }
