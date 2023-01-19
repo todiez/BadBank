967ae4e02a4457842fe47bceafc8a78f686935ee
tybo-dev <tobias@diez.guru>
2023-01-19 09:40:42 +0100

    context added

--- context.js
+++ context.js
@@ -11,8 +11,7 @@ function Card(props) {
     return "card mb-3 " + bg + txt;
   }
 
-  return (
-  
+  return (  
     <div className={classes()} style={{ maxWidth: "18rem", margin: "50px"}}>
       <h5 className="card-header" style={{background: "#AED6F1" }}>{props.header}</h5>
       <div className="card-body" style={{background: "#EBF5FB" }}>
