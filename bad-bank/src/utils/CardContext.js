68f7c3f01ea27b954ff33605b2de34f6de577135
tybo-dev <tobias@diez.guru>
2023-02-04 10:30:39 +0100

    card testing

--- bad-bank/src/utils/CardContext.js
+++ bad-bank/src/utils/CardContext.js
@@ -1,4 +1,9 @@
+import { createContext } from "react";
+
 //card component as shared context
+const CardContext = createContext(null);
+
+
 function Card(props) {
   function classes() {
     const bg = props.bgcolor ? "bg-" + props.bgcolor : " ";
@@ -7,15 +12,14 @@ function Card(props) {
   }
 
   return (  
-    <div className={classes()} style={{ maxWidth: "18rem", margin: "50px"}}>
+    <div className="card mb-3" style={{ maxWidth: "18rem", margin: "50px"}}>
       <h5 className="card-header" style={{background: "#AED6F1" }}>{props.header}</h5>
       <div className="card-body" style={{background: "#EBF5FB" }}>
         {props.title && <h5 className="card-title">{props.title}</h5>}
         {props.text && <p className="card-text">{props.text}</p>}
         {<hr/>}
-        {/* {<br /> } */}
         {props.body}
-        {props.status && <div id="createStatus">{props.status}</div>}
+        {/* {props.status && <div id="createStatus">{props.status}</div>} */}
       </div>
     </div>
   );
